462f479918c74c9af8f2bb2bde1b0aed04e31b37
Rui Martins <ruimartinsptl@gmail.com>
2018-09-08 14:47:58 +0100

    up

--- Vision/20-ImageShape.py
+++ Vision/20-ImageShape.py
@@ -11,8 +11,8 @@ print "height: %d pixels" % (image.shape[0])
 print "channels: %d" % (image.shape[2])
 
 # show the image and wait for a keypress
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+# cv2.imshow("Image", image)
+# cv2.waitKey(0)
 
 # save the image -- OpenCV handles converting filetypes
 # automatically
